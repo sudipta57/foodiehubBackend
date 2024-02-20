10d4570bd312505e2d0376857a2e1b93445447be
sudipta57 <ghoramiswapna32@gmail.com>
2024-02-20 20:24:03 +0530

    app.js cors for removing localhost

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://foodiehub-getyourgoodfood.netlify.app",
+    origin: "https://foodiehub-getyourgoodfoods.netlify.app",
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     credentials: true,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
