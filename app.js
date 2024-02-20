6ceea96b0fb0d1e0e277605c237bca604a8fc3d3
sudipta57 <ghoramiswapna32@gmail.com>
2024-02-20 20:04:23 +0530

    app.js cors for localhost

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 const cors = require("cors");
 const cookieparser = require("cookie-parser");
 app.use(express.json());
+
 app.use(
   cors({
-    origin: "https://foodiehub-getyourgoodfood.netlify.app",
+    origin: [
+      "https://foodiehub-getyourgoodfood.netlify.app",
+      "http://localhost:5173",
+    ],
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     credentials: true,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
